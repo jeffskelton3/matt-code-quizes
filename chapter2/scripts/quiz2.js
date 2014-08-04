ec99462e98a25fa9d25cdc0b1395f03e08e3a9ee
Matthew <mattheweldredge@gmail.com>
2014-08-04 15:15:50 -0700

    Quiz 2 answer 4

--- chapter2/scripts/quiz2.js
+++ chapter2/scripts/quiz2.js
@@ -4,3 +4,5 @@ console.log($('#example_1_1 span').text());
 console.log($('#example_1_1').html());
 /* Question 3 answer */
 $("#example_1_2 span").text('replaced!');
+/* Question 4 answer */
+$("#example_1_2").html('<a href="http://google.com">click me to go to Google</a>');
