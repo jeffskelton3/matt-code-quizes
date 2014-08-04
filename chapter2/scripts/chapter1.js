ff42e9b1ec9403272302666d610175f9e28543f6
Matthew <mattheweldredge@gmail.com>
2014-08-04 13:07:49 -0700

    chapter quiz 2 answer 1

--- chapter2/scripts/chapter1.js
+++ chapter2/scripts/chapter1.js
@@ -1,4 +1,5 @@
 $("li").addClass("red");
 $("li:odd").addClass("odd");
 $("li:nth-child(5)").text("foobar");
-$("li:contains('fourth line')").remove();
+$("li:contains(\"fourth line\")").remove();
+$('input[type=text]').addClass('red');
