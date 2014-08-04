65e353f8cd09294367cade16330558695bc32c68
Matthew <mattheweldredge@gmail.com>
2014-08-04 14:40:20 -0700

    quiz 2 answer 3

--- chapter2/scripts/quiz2.js
+++ chapter2/scripts/quiz2.js
@@ -1,4 +1,6 @@
 /* Question 1 answer */
 console.log($('#example_1_1 span').text());
 /* Question 2 answer */
-console.log($(' div #example_1_1').html());
+console.log($('#example_1_1').html());
+/* Question 3 answer */
+$("#example_1_2 span").text('replaced!');
