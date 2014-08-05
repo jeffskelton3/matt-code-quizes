f5fe563fa42f89ec9828f69c713e6aa97526b377
Matthew <mattheweldredge@gmail.com>
2014-08-05 16:41:40 -0700

    quiz 2 question 7 update

--- chapter2/scripts/quiz2.js
+++ chapter2/scripts/quiz2.js
@@ -1,35 +1,36 @@
 /* Question 1 answer */
 console.log($('#example_1_1 span').text());
+
 /* Question 2 answer */
 console.log($('#example_1_1').html());
+
 /* Question 3 answer */
 $("#example_1_2 span").text('replaced!');
+
 /* Question 4 answer */
 $("#example_1_2").html('<a href="http://google.com">click me to go to Google</a>');
+
 /* Question 5 answer */
 $('#example_1_3 span').before('<img src="http://9pixs.com/wp-content/uploads/2014/07/chiwawa-puppies_1404253665.jpg">');
+
 /* Question 6 answer */
 $('#example_1_4').remove(); /* .empty(); */
+
 /* Question 7 answer */
 $(document).ready(function() {
     // put all your jQuery goodness in here.
 });
+
 /* Question 8 answer */
-var grizzlyDerringer = {
-	members : [Dave, Jason, Matt, Jeff],
-	drums : 'Dave',
-	bass : 'Jason',
-	guitar1 : 'Matt',
-	guitar2 : 'Jeff' 
-	playDrums : function(drums) {
-		alert('Dave plays' + 'drums' + 'in Grizzly Derringer!')
-	}
-}
+
+
+
 
 /* Question 9 answer */
 a function without a name
 an anonymous function can be stored in a variable and called like any function created with a function declaration
 
+/* Question 10 answer */
 $( "li" ).each(function( index ) {
   console.log( index + ": " + $( this ).text() );
 });
